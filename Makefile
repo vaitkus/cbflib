vaitkus/cbflib

commit 81f912dc9951fcdf504908ce4996b10d5d408609

Revert "Merge pull request #95 from hattne/mkstemp"
This reverts commit feeac197d12b87eea07a6178dafd4b456db7a71e, reversing
changes made to 2cb404e643ffebecd8a16087a76571d61ad8ae98.

Makefile
@@ -733,9 +733,9 @@ endif
 CC	= gcc
 C++	= g++
 ifneq ($(CBFDEBUG),)
-CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DCBFDEBUG=1 -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DCBFDEBUG=1 -DHAVE_UNISTD_H $(HDF5CFLAGS)
 else
-CFLAGS  = -g -O3 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O3 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 endif
 LDFLAGS =
 F90C = gfortran

m4/Makefile.m4
@@ -739,7 +739,7 @@ endif
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -763,7 +763,7 @@ cbf_system,`OSX_gcc42',`
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -787,7 +787,7 @@ cbf_system,`OSX_gcc42_DMALLOC',`
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DDMALLOC -DDMALLOC_FUNC_CHECK -I$(HOME)/include -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DDMALLOC -DDMALLOC_FUNC_CHECK -I$(HOME)/include -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -813,7 +813,7 @@ cbf_system,`LINUX_64',`
 #########################################################
 CC	= gcc -m64
 C++	= g++ -m64
-CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran -m64
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -841,7 +841,7 @@ cbf_system,`LINUX_gcc42',`
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -868,7 +868,7 @@ cbf_system,`LINUX',`
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -897,7 +897,7 @@ cbf_system,`LINUX_gcc42_DMALLOC', `
 CC	= gcc
 C++	= g++
 CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing \
-	  -DDMALLOC -DDMALLOC_FUNC_CHECK -DHAVE_MKSTEMP -DHAVE_REALPATH \
+	  -DDMALLOC -DDMALLOC_FUNC_CHECK -DHAVE_REALPATH \
 	  -DHAVE_UNISTD_H $(HDF5CFLAGS) -I$(HOME)/include
 LDFLAGS =
 F90C = gfortran
@@ -925,7 +925,7 @@ cbf_system,`LINUX_DMALLOC',`
 CC	= gcc
 C++	= g++
 CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing \
-	  -DDMALLOC -DDMALLOC_FUNC_CHECK -DHAVE_MKSTEMP -DHAVE_REALPATH \
+	  -DDMALLOC -DDMALLOC_FUNC_CHECK -DHAVE_REALPATH \
 	  -DHAVE_UNISTD_H $(HDF5CFLAGS) -I$(HOME)/include
 LDFLAGS =
 F90C = gfortran
@@ -1042,8 +1042,8 @@ cbf_system,`MSYS2',`
 CC	= gcc
 C++	= g++
 CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -DH5_HAVE_WIN32_API \
-  -DH5_HAVE_MINGW -DH5_USE_110_API -fno-strict-aliasing -DHAVE_MKSTEMP \
-  -DHAVE_UNISTD_H $(HDF5CFLAGS)
+  -DH5_HAVE_MINGW -DH5_USE_110_API -fno-strict-aliasing -DHAVE_UNISTD_H \
+  $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz
@@ -1099,9 +1099,9 @@ RANLIB  = ',
 CC	= gcc
 C++	= g++
 ifneq ($(CBFDEBUG),)
-CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DCBFDEBUG=1 -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DCBFDEBUG=1 -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 else
-CFLAGS  = -g -O3 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O3 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 endif
 LDFLAGS =
 F90C = gfortran

CMakeLists.txt
@@ -345,18 +345,13 @@ if(HAVE_UNISTD_H)
 endif()
 
 
-# Check for missing functions: fgetln(3) is in 4.4BSD; mkstemp(3) is
-# in 4.3BSD, POSIX.1-2001; realpath(3) is in 4.4BSD, POSIX.1-2001;
-# regex(3) is in POSIX.1-2001, POSIX.1-2008.
+# Check for missing functions: fgetln(3) is in 4.4BSD; realpath(3) is
+# in 4.4BSD, POSIX.1-2001; regcomp(3) is in POSIX.1-2001,
+# POSIX.1-2008.
 include(CheckSymbolExists)
 check_symbol_exists(fgetln "stdio.h" HAVE_FGETLN)
 if(HAVE_FGETLN)
   add_compile_definitions("HAVE_FGETLN")
-endif()
-
-check_symbol_exists(mkstemp "stdlib.h" HAVE_MKSTEMP)
-if(HAVE_MKSTEMP)
-  add_compile_definitions("HAVE_MKSTEMP")
 endif()
 
 check_symbol_exists(realpath "stdlib.h" HAVE_REALPATH)
@@ -1121,10 +1116,6 @@ add_executable(cbf2nexus
   "${CBF__EXAMPLES}/cbf2nexus.c")
 target_link_libraries(cbf2nexus
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(cbf2nexus
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(nexus2cbf
   "${CBF__EXAMPLES}/nexus2cbf.c")
@@ -1136,10 +1127,6 @@ add_executable(minicbf2nexus
   "${CBF__EXAMPLES}/minicbf2nexus.c")
 target_link_libraries(minicbf2nexus
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(minicbf2nexus
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(adscimg2cbf
   "${CBF__EXAMPLES}/adscimg2cbf.c"
@@ -1162,19 +1149,11 @@ add_executable(convert_image
   "${CBF__EXAMPLES}/convert_image.c")
 target_link_libraries(convert_image
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(convert_image
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(convert_minicbf
   "${CBF__EXAMPLES}/convert_minicbf.c")
 target_link_libraries(convert_minicbf
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(convert_minicbf
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(makecbf
   "${CBF__EXAMPLES}/makecbf.c")
@@ -1211,19 +1190,6 @@ add_executable(img2cif
   "${CBF__EXAMPLES}/img2cif.c")
 target_link_libraries(img2cif
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(img2cif
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
-
-add_executable(cif2c
-  "${CBF__EXAMPLES}/cif2c.c")
-target_link_libraries(cif2c
-  cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(cif2c
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(cif2cbf
   "${CBF__EXAMPLES}/cif2cbf.c")
@@ -1231,10 +1197,6 @@ target_link_libraries(cif2cbf
   cbf
   CQR
   "${libm}")
-if(NOT HAVE_MKSTEMP)
-  target_sources(cif2cbf
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(cbf_template_t
   "${CBF__DECTRIS_EXAMPLES}/cbf_template_t.c")
@@ -1262,10 +1224,6 @@ add_executable(sequence_match
   "${CBF__EXAMPLES}/sequence_match.c")
 target_link_libraries(sequence_match
   cbf)
-if(NOT HAVE_MKSTEMP)
-  target_sources(sequence_match
-    PRIVATE "${CBF__SRC}/mkstemp.c")
-endif()
 
 add_executable(test_cbf_airy_disk
   "${CBF__EXAMPLES}/test_cbf_airy_disk.c")

Makefile_LINUX
@@ -732,7 +732,7 @@ endif
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz

Makefile_MSYS2
@@ -733,8 +733,8 @@ endif
 CC	= gcc
 C++	= g++
 CFLAGS  = -g -O2 -Wall -D_USE_XOPEN_EXTENDED -DH5_HAVE_WIN32_API \
-  -DH5_HAVE_MINGW -DH5_USE_110_API -fno-strict-aliasing -DHAVE_MKSTEMP \
-  -DHAVE_UNISTD_H $(HDF5CFLAGS)
+  -DH5_HAVE_MINGW -DH5_USE_110_API -fno-strict-aliasing -DHAVE_UNISTD_H \
+  $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz

Makefile_OSX
@@ -732,7 +732,7 @@ endif
 #########################################################
 CC	= gcc
 C++	= g++
-CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_MKSTEMP -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O2 -Wall -std=c99 -pedantic -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 LDFLAGS =
 F90C = gfortran
 #F90FLAGS = -g -fno-range-check -fallow-invalid-boz

examples/cbf2nexus.c
@@ -259,10 +259,9 @@
 
 #define C2CBUFSIZ 8192
 
-#ifndef HAVE_MKSTEMP
-#define mkstemp _cbf_mkstemp
-int
-_cbf_mkstemp(char *templ);
+#ifdef __MINGW32__
+#define NOMKSTEMP
+#define NOTMPDIR
 #endif
 
 int local_exit (int status);
@@ -651,13 +650,26 @@ int main (int argc, char *argv [])
         
         for (f = 0; CBF_SUCCESS == error && f != cifid; ++f) {
 			cbf_handle cif = NULL;
+#ifdef NOTMPDIR
+			char ciftmp[] = "cif2cbfXXXXXX";
+#else
 			char ciftmp[] = "/tmp/cif2cbfXXXXXX";
     		int ciftmpfd;
+#endif
 			/* Get suitable file - reading from stdin to a temporary file if needed */
 			if (!(cifin[f]) || strcmp(cifin[f]?cifin[f]:"","-") == 0) {
 				FILE *file = NULL;
 				int nbytes;
 				char buf[C2CBUFSIZ];
+#ifdef NOMKSTEMP
+				if (mktemp(ciftmp) == NULL ) {
+                    fprintf(stderr,"%s: Can't create temporary file name %s.\n%s\n", argv[0], ciftmp,strerror(errno));
+					error |= CBF_FILEOPEN;
+				} else if ((file = fopen(ciftmp,"wb+")) == NULL) {
+                    fprintf(stderr,"Can't open temporary file %s.\n%s\n", ciftmp,strerror(errno));
+					error |= CBF_FILEOPEN;
+                }
+#else
                 if ((ciftmpfd = mkstemp(ciftmp)) == -1 ) {
                     fprintf(stderr,"%s: Can't create temporary file %s.\n%s\n", argv[0], ciftmp,strerror(errno));
 					error |= CBF_FILEOPEN;
@@ -665,6 +677,7 @@ int main (int argc, char *argv [])
                     fprintf(stderr,"Can't open temporary file %s.\n%s\n", ciftmp,strerror(errno));
 					error |= CBF_FILEOPEN;
                 }
+#endif
                 while ((nbytes = fread(buf, 1, C2CBUFSIZ, stdin))) {
                     if((size_t)nbytes != fwrite(buf, 1, nbytes, file)) {
                         fprintf(stderr,"Failed to write %s.\n", ciftmp);

examples/cif2c.c
@@ -309,10 +309,9 @@
 
 #define C2CBUFSIZ 8192
 
-#ifndef HAVE_MKSTEMP
-#  define mkstemp _cbf_mkstemp
-int
-_cbf_mkstemp(char *templ);
+#ifdef __MINGW32__
+#define NOMKSTEMP
+#define NOTMPDIR
 #endif
 
 
@@ -341,7 +340,12 @@ int main (int argc, char *argv [])
   int errflg = 0;
   const char *cifin, *codeout, *function_name;
   char ciftmp[19];
+#ifdef NOMKSTEMP
+  char *xciftmp;
+#endif
+#ifndef NOMKSTEMP
   int ciftmpfd;
+#endif
   int ciftmpused;
   unsigned int nbytes;
   char buf[C2CBUFSIZ];
@@ -430,7 +434,23 @@ int main (int argc, char *argv [])
     /* Read the cif */
 
    if (!cifin || strcmp(cifin?cifin:"","-") == 0) {
+#ifdef NOTMPDIR
+     strcpy(ciftmp, "cif2cXXXXXX");
+#else
      strcpy(ciftmp, "/tmp/cif2cXXXXXX");
+#endif
+#ifdef NOMKSTEMP
+     if ((xciftmp=mktemp(ciftmp)) == NULL ) {
+       fprintf(stderr,"\n cif2c: Can't create temporary file name %s.\n", ciftmp);
+       fprintf(stderr,"%s\n",strerror(errno));
+       exit(1);
+     }
+     if ( (file = fopen(ciftmp,"wb+")) == NULL) {
+       fprintf(stderr,"Can't open temporary file %s.\n", ciftmp);
+       fprintf(stderr,"%s\n",strerror(errno));
+       exit(1);     	
+     }
+#else
      if ((ciftmpfd = mkstemp(ciftmp)) == -1 ) {
        fprintf(stderr,"Can't create temporary file %s.\n", ciftmp);
        fprintf(stderr,"%s\n",strerror(errno));
@@ -441,6 +461,7 @@ int main (int argc, char *argv [])
        fprintf(stderr,"%s\n",strerror(errno));
        exit(1);
      }
+#endif
      while ((nbytes = fread(buf, 1, C2CBUFSIZ, stdin))) {
        if(nbytes != fwrite(buf, 1, nbytes, file)) {
          fprintf(stderr,"Failed to write %s.\n", ciftmp);

commit 822cd4fab2a73820859f814c83c86d501c54dd09

Synchronize Makefiles Revert e5a992f95112659a66fe2a625602597a858d4ccf.  Fix inconsistency introduced in 81f912dc9951fcdf504908ce4996b10d5d408609.

Makefile
@@ -733,7 +733,7 @@ endif
 CC	= gcc
 C++	= g++
 ifneq ($(CBFDEBUG),)
-CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DCBFDEBUG=1 -DHAVE_UNISTD_H $(HDF5CFLAGS)
+CFLAGS  = -g -O0 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DCBFDEBUG=1 -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 else
 CFLAGS  = -g -O3 -Wall -D_USE_XOPEN_EXTENDED -fno-strict-aliasing -DHAVE_REALPATH -DHAVE_UNISTD_H $(HDF5CFLAGS)
 endif

m4/Makefile.m4
@@ -1917,7 +1917,7 @@ build_py3ply:   $(M4)/Makefile.m4
 	touch build_py3ply
 $(PY3PLY):      build_py3ply
 	-rm -rf $(PY3PLY)
-	git clone $(PY3PLYURL)
+	wget -O - -nv $(PY3PLYURL) | tar -xzf -
 	(cd $(PY3PLY); \
 	PYTHONPATH=$(PY3CIFRW_PREFIX)/lib/python:$(PY3CIFRW_PREFIX)/lib64/python; export PYTHONPATH; \
 	mkdir -p $(PY3CIFRW_PREFIX)/lib/python/site-packages; \